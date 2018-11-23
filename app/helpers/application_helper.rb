2ca63d8df408821082ae8eb050e6f610af45cb5d
Nick Muerdter <nick.muerdter@nrel.gov>
2018-11-23 15:07:09 -0700

    Add information to footer about free Heroku database limits.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,4 +13,20 @@ module ApplicationHelper
       "alert-#{flash_type}"
     end
   end
+
+  def database_rows
+    @database_rows ||= ActiveRecord::Base.connection.select_value("SELECT SUM(n_live_tup) FROM pg_stat_user_tables").to_i
+  end
+
+  def database_rows_limit
+    10000
+  end
+
+  def database_rows_percent
+    @database_rows_percent ||= ((database_rows / database_rows_limit.to_f) * 100).round
+  end
+
+  def database_size
+    @database_size ||= ActiveRecord::Base.connection.select_value("SELECT pg_table_size('pg_largeobject')").to_i
+  end
 end
