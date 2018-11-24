c55691ee06bbf775ced929acb0b05920fe15ee63
Nick Muerdter <nick.muerdter@nrel.gov>
2018-11-23 20:39:23 -0700

    Styling tweaks for table and pagination.

--- app/javascript/font_awesome_icons.js
+++ app/javascript/font_awesome_icons.js
@@ -2,6 +2,9 @@ import { library, dom } from '@fortawesome/fontawesome-svg-core';
 import { faFile } from '@fortawesome/free-solid-svg-icons/faFile';
 import { faPencilAlt } from '@fortawesome/free-solid-svg-icons/faPencilAlt';
 import { faPlus } from '@fortawesome/free-solid-svg-icons/faPlus';
+import { faSort } from '@fortawesome/free-solid-svg-icons/faSort';
+import { faSortDown } from '@fortawesome/free-solid-svg-icons/faSortDown';
+import { faSortUp } from '@fortawesome/free-solid-svg-icons/faSortUp';
 import { faSyncAlt } from '@fortawesome/free-solid-svg-icons/faSyncAlt';
 import { faTimes } from '@fortawesome/free-solid-svg-icons/faTimes';
 
@@ -9,6 +12,9 @@ library.add(
   faFile,
   faPencilAlt,
   faPlus,
+  faSort,
+  faSortDown,
+  faSortUp,
   faSyncAlt,
   faTimes,
 );
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,11 @@ import $ from 'jquery';
 
 Rails.start();
 
+$.fn.DataTable.defaults.headerCallback = function(thead) {
+  $(thead).find('th:not(.sort-arrows-added)').append('<i class="fas fa-sort"></i><i class="fas fa-sort-up"></i><i class="fas fa-sort-down"></i>');
+  $(thead).find('th').addClass('sort-arrows-added');
+};
+
 $(document).ready(function(){
   $('table.table-data-tables').DataTable({
     pageLength: 50,
